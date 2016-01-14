50d0b8fa80cd7461ea5dd428fc90535db69dade0
deibidc <deibidc@gmail.com>
2016-01-14 15:42:16 -0600

    aplicaciones web y android notificaciones push

---
+++ Base de Datos/gcm.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-01-2016 a las 22:51:28
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `gcm`
+--
+CREATE DATABASE IF NOT EXISTS `gcm` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `gcm`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `gcm_users`
+--
+
+CREATE TABLE IF NOT EXISTS `gcm_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gcm_regid` text,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Pagina WEB/gcm/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "HOST");
+define("DB_USER", "USER");
+define("DB_PASSWORD", "PASSWORD");
+define("DB_DATABASE", "DATA BASE");
+/*
+ * Google API Key
+ */
+define("GOOGLE_API_KEY", "API KEY (SERVER)"); // Place your Google API Key
+?>
